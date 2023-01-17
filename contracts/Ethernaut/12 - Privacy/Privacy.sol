7db6c1bfcfcf3ed46ad8d14213be33847a76807b
stalin_laptop <mags961224hjccmt00@gmail.com>
2023-01-16 19:21:08 -0600

    Documented the storage composition in the Privacy contract
    what is stored on each slot of the contract's storage?

--- contracts/Ethernaut/12 - Privacy/Privacy.sol
+++ contracts/Ethernaut/12 - Privacy/Privacy.sol
@@ -10,6 +10,15 @@ contract Privacy {
   uint16 private awkwardness = uint16(block.timestamp);
   bytes32[3] private data;
 
+  /*
+  * Slot 0 has the locked variable
+  * Slot 1 has the ID variable
+  * Slot 2 has the 2 uint8 vars and the uin16 var -> The 3 combined takes up the 32 bytes available in the entire second slot of the storage
+  * Slot 3 has the first bytes32 of data   --> data[0]
+  * Slot 4 has the second bytes32 of data  --> data[1]
+  * Slot 5 has the third bytes32 of data   --> data[2]  <---> Wich in theory stored the key!
+  */
+
   constructor(bytes32[3] memory _data) {
     data = _data;
   }
